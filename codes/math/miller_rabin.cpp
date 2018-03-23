e8b7a29f5dbad39605526d6ec2ada40657ed845e
polarz <harry900831@gmail.com>
2018-03-23 16:50:38 +0800

    update!

--- codes/math/miller_rabin.cpp
+++ codes/math/miller_rabin.cpp
@@ -3,26 +3,11 @@
 //num = x*2^t
 int p[3] = {2,7,61};
 
-<<<<<<< HEAD:codes/math/Miller_Rabin.cpp
-=======
-lld fast_power(lld a,lld b,lld c = 1000000007){
-	lld ret = 1;
-	a %= c;
-	while(b){
-		if(b&1)ret = (ret*a)%c;
-		a = (a*a)%c;
-		b >>= 1;
-	}
-	return ret;
-}
-
->>>>>>> 40cd3aec4312367047379ef40bce0c0b843ee150:codes/math/miller_rabin.cpp
 bool miller_rabin(long long num){
 	long long t = 0,x = num-1;
 	while(x % 2 == 0)t++,x /= 2;
 	bool is = 1;
 	for(int i = 0 ; i < 3 && p[i] < num; i++){
-<<<<<<< HEAD:codes/math/Miller_Rabin.cpp
         lld v = fast_power(p[i],x,num);
         if(v == 1 || v == num-1)continue;
         bool tt = 0;
@@ -34,7 +19,6 @@ bool miller_rabin(long long num){
             }
         }
         if(!tt)is = 0;
-=======
 		lld v = fast_power(p[i],x,num);
 		if(v == 1 || v == num-1)continue;
 		bool tt = 0;
@@ -46,7 +30,6 @@ bool miller_rabin(long long num){
 			}
 		}
 		if(!tt)is = 0;
->>>>>>> 40cd3aec4312367047379ef40bce0c0b843ee150:codes/math/miller_rabin.cpp
 	}
 	return is;
 }
