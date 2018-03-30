489d5555f9b55be66b7fb37721b04d68a18147a5
polarz <harry900831@gmail.com>
2018-03-30 20:58:20 +0800

    update

---
+++ codes/Basic/template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define lld long long
+#define PB push_back
+#define F first
+#define S second
+#define jizz cin.tie(0);ios_base::sync_with_stdio(0);
+#define endl '\n'
+using namespace std;
+typedef pair<int,int> Pair;
+
+int main(){jizz
+	
+	return 0;
+}
+
